1b5473a0caa0a364b234514a1320fe023d7b5eac
Bruno Lopes <brunomlopes@gmail.com>
2014-10-18 01:29:38 +0100

    Better map reduce

--- SqlSaturdayCode/SqlSaturdayCode/Models/SessionsPerSpeaker.cs
+++ SqlSaturdayCode/SqlSaturdayCode/Models/SessionsPerSpeaker.cs
@@ -1,50 +1,48 @@
-﻿using Raven.Abstractions.Indexing;
+﻿using System.Linq;
+using Raven.Abstractions.Indexing;
 using Raven.Client.Indexes;
 
 namespace SqlSaturdayCode.Models
 {
-    public class SessionsPerSpeaker : AbstractIndexCreationTask
+    public class SessionsPerSpeaker :
+        AbstractMultiMapIndexCreationTask<SessionsPerSpeaker.Result>
     {
+        public SessionsPerSpeaker()
+        {
+            AddMap<Speaker>(speakers => from speaker in speakers
+                select new
+                {
+                    SpeakerId = speaker.Id,
+                    SpeakerName = speaker.Name,
+                    NumberOfSessions = 0
+                });
+            AddMap<Event>(events => from evt in events
+                from session in evt.Sessions
+                select new
+                {
+                    SpeakerId = session.SpeakerId,
+                    SpeakerName = (string) null,
+                    NumberOfSessions = 1
+                });
+
+            Reduce = results => from result in results
+                group result by result.SpeakerId
+                into g
+                select new
+                {
+                    SpeakerId = g.Key,
+                    SpeakerName = g.Where(l => l.SpeakerName != null)
+                        .Select(l => l.SpeakerName)
+                        .FirstOrDefault(),
+                    NumberOfSessions = g.Sum(l => l.NumberOfSessions)
+                };
+        }
+
         public class Result
         {
             public string SpeakerId { get; set; }
             public string SpeakerName { get; set; }
             public int NumberOfSessions { get; set; }
-        }
-
-        public override string IndexName
-        {
-            get { return "SessionsPerSpeaker"; }
-        }
-
-        public override IndexDefinition CreateIndexDefinition()
-        {
-            return new IndexDefinition
-            {
-                Maps =
-                {
-                    @"from speaker in docs.Speakers
-select new {
-	SpeakerId = speaker.Id,
-	SpeakerName = speaker.Name,
-	NumberOfSessions = 0
-}",
-                    @"from evt in docs.Events
-from session in evt.Sessions
-select new {
-	SpeakerId = session.SpeakerId,
-	SpeakerName = (string)null,
-	NumberOfSessions = 1
-}"
-                },
-                Reduce = @"from result in results
-group result by result.SpeakerId into g
-select new {
-	SpeakerId = g.Key,
-	SpeakerName = g.Where(l => l.SpeakerName != null).Select(l => l.SpeakerName).FirstOrDefault(),
-	NumberOfSessions = g.Sum(l => l.NumberOfSessions)
-}"
-            };
         }
     }
 }
